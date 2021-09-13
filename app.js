d3a9c3807b4645ede05ee250e88045e39d3cc1f6
pablopsdigital <pablopsdigital@gmail.com>
2021-09-13 15:06:40 +0200

    Layout initial templates views

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function (err, req, res, next) {
 
   // Render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("pages/error");
 });
 
 //==================================================================
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,110 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  var advertisementList = [
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "bike.jpg",
+      tags: ["Lifestyle", " Mobile"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: false,
+      price: 230.53,
+      photo: "iphone.png",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "surf.webp",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "bike.jpg",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: false,
+      price: 230.53,
+      photo: "iphone.png",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "surf.webp",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "bike.jpg",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: false,
+      price: 230.53,
+      photo: "iphone.png",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "surf.webp",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "bike.jpg",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: false,
+      price: 230.53,
+      photo: "iphone.png",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+    {
+      name: "Titulo anuncio",
+      sale: true,
+      price: 230.53,
+      photo: "bike.jpg",
+      tags: ["tag1", " tag2"],
+      createdAt: "2021-09-11",
+    },
+  ];
+  res.render("pages/index", {
+    title: "Express",
+    advertisementList: advertisementList,
+  });
 });
 
 module.exports = router;
